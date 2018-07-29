e2aa2e4a66cc243f29c2642e9d7da8b14aa63a44
BrainStone <yannick@tedworld.de>
2018-07-29 20:26:13 +0200

    More test cases

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,23 @@
 // Licensed under the GPLv3 License. See LICENSE file in the project root for full license information.
 //
 #include <iostream>
+#include <string>
 
 #include <gmpxx.h>
 
 #include "primality.hpp"
 
+void do_primnecheck( mpz_class num );
+
 int main() {
-	mpz_class prime1( "216567629137" );
-	mpz_class prime2( "357686312646216567629137" );
+	const std::string number( "357686312646216567629137" );
 
-	std::cout << prime1 << " has " << mpz_sizeinbase( prime1.get_mpz_t(), 2 ) << " bits" << std::flush;
-	std::cout << " and " << (is_prime( prime1 ) ? "is" : "is not") << " a prime." << std::endl;
+	for ( int i = number.size() - 1; i >= 0; --i ) {
+		do_primnecheck( mpz_class( std::string( number, 0, i ) ) );
+	}
+}
 
-	std::cout << prime2 << " has " << mpz_sizeinbase( prime2.get_mpz_t(), 2 ) << " bits" << std::flush;
-	std::cout << " and " << (is_prime( prime2 ) ? "is" : "is not") << " a prime." << std::endl;
+void do_primnecheck( mpz_class num ) {
+	std::cout << num << " has " << mpz_sizeinbase( num.get_mpz_t(), 2 ) << " bits" << std::flush;
+	std::cout << " and " << (is_prime( num ) ? "is" : "is not") << " a prime." << std::endl;
 }
