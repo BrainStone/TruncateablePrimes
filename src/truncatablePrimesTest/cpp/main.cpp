c3cf613f3b0d01009ce2f36bad926c0c1d28d65d
BrainStone <yannick@tedworld.de>
2018-07-29 23:19:49 +0200

    First test case

---
+++ src/truncatablePrimesTest/cpp/main.cpp
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2018 Yannick Schinko. All rights reserved.
+// Licensed under the GPLv3 License. See LICENSE file in the project root for full license information.
+//
+#include <gtest/gtest.h>
+
+int main( int argc, char **argv ) {
+	::testing::InitGoogleTest( &argc, argv );
+	return RUN_ALL_TESTS();
+}
---
+++ src/truncatablePrimesTest/cpp/primality.cpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2018 Yannick Schinko. All rights reserved.
+// Licensed under the GPLv3 License. See LICENSE file in the project root for full license information.
+//
+#include <gtest/gtest.h>
+
+#include "primality.hpp"
+
+TEST( PrimalityTest, SimplePrimes ) {
+	EXPECT_FALSE( is_prime( 123_mpz ) );
+	EXPECT_FALSE( is_prime( 121_mpz ) );
+	EXPECT_TRUE( is_prime( 127_mpz ) );
+
+	EXPECT_FALSE( is_prime( 566547_mpz ) );
+	EXPECT_TRUE( is_prime( 566549_mpz ) );
+
+	EXPECT_FALSE( is_prime( 2147483641_mpz ) );
+	EXPECT_TRUE( is_prime( 2147483647_mpz ) );
+
+	EXPECT_FALSE( is_prime( 566547_mpz ) );
+	EXPECT_TRUE( is_prime( 2305843009213693943_mpz ) );
+}
