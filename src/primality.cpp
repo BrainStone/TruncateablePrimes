d2879e9b833279424208614a8cb4b811883fc20c
BrainStone <yannick@tedworld.de>
2018-07-29 20:35:30 +0200

    Verbose debugging

--- src/primality.cpp
+++ src/primality.cpp
@@ -9,7 +9,11 @@ bool is_prime( const mpz_class& number ) {
 	uint64_t root = sqrt( number ).get_val().get_ui();
 	primesieve::iterator it;
 
+	std::clog << "\tRoot:" << root << std::endl;
+
 	while ( (prime = it.next_prime()) <= root )
+		std::clog << "\t\tPrime:" << prime << std::endl;
+
 		if ( number % prime == 0 )
 			return false;
 
