ee577d442beb0c99d9e79034bc9f4fe847cdc500
BrainStone <yannick@tedworld.de>
2018-07-30 22:43:22 +0200

    Added some tougher negative test cases
    (123456th twin primes are unknown)

--- src/truncatablePrimesTest/cpp/primality.cpp
+++ src/truncatablePrimesTest/cpp/primality.cpp
@@ -20,3 +20,16 @@ TEST( PrimalityTest, SimplePrimes ) {
 	EXPECT_FALSE( is_prime( 566547_mpz ) );
 	EXPECT_TRUE( is_prime( 2305843009213693951_mpz ) );
 }
+
+TEST( PrimalityTest, TwinPrimeProducts ) {
+	// 1st twin prime pair
+	EXPECT_FALSE( is_prime( 3_mpz * 5_mpz ) );
+	// 12th twin prime pair
+	EXPECT_FALSE( is_prime( 149_mpz * 151_mpz ) );
+	// 123th twin prime pair
+	EXPECT_FALSE( is_prime( 4787_mpz * 4789_mpz ) );
+	// 1234th twin prime pair
+	EXPECT_FALSE( is_prime( 101207_mpz * 101209_mpz ) );
+	// 12345th twin prime pair
+	EXPECT_FALSE( is_prime( 1612181_mpz * 1612183_mpz ) );
+}
