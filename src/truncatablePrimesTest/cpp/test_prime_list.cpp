3034644f53e76ec0f1cf3addd18751df9518f850
BrainStone <yannick@tedworld.de>
2018-08-05 17:24:01 +0200

    Implemented test for prime_list

---
+++ src/truncatablePrimesTest/cpp/test_prime_list.cpp
@@ -0,0 +1,89 @@
+//
+// Copyright (c) 2018 Yannick Schinko. All rights reserved.
+// Licensed under the GPLv3 License. See LICENSE file in the project root for full license information.
+//
+#include "global_test_settings.hpp"
+
+#include <atomic>
+#include <random>
+#include <thread>
+
+#include "prime_list.hpp"
+#include "truncate.hpp"
+
+const std::uniform_int_distribution<int> all_digits( 0, 9 );
+
+TEST( PrimeListTest, Order ) {
+	prime_list<int> list;
+
+	list.insert( 48 );
+	list.insert( -425 );
+	list.insert( 78 );
+	list.insert( 4652 );
+
+	prime_list<int>::iterator it( list.begin() );
+
+	EXPECT_EQ( *it++, -425 );
+	EXPECT_EQ( *it++, 48 );
+	EXPECT_EQ( *it++, 78 );
+	EXPECT_EQ( *it++, 4652 );
+	EXPECT_EQ( it, list.end() );
+
+	prime_list<int>::reverse_iterator rit( list.rbegin() );
+
+	EXPECT_EQ( *rit++, 4652 );
+	EXPECT_EQ( *rit++, 78 );
+	EXPECT_EQ( *rit++, 48 );
+	EXPECT_EQ( *rit++, -425 );
+	EXPECT_EQ( rit, list.rend() );
+}
+
+constexpr int count = 10000;
+
+void helper( prime_list<>& list, int digit ) {
+	std::default_random_engine generator( digit );
+	mpz_class num( digit );
+
+	EXPECT_TRUE( list.insert( num ).second );
+
+	for ( int i = 0; i < count; ++i ) {
+		num = append_right( num, all_digits( generator ) );
+
+		EXPECT_TRUE( list.insert( num ).second );
+	}
+}
+
+TEST( PrimeListTest, Concurrency ) {
+	prime_list<> list;
+
+	std::thread early_threads[7];
+
+	for ( int i = 0; i < 7; ++i ) {
+		early_threads[i] = std::thread( [&, i] { helper( list, i + 1 ); } );
+	}
+
+	for ( std::thread& thread : early_threads ) {
+		thread.join();
+	}
+
+	std::thread late_threads[2];
+
+	for ( int i = 0; i < 2; ++i ) {
+		late_threads[i] = std::thread( [&, i] { helper( list, i + 8 ); } );
+	}
+
+	for ( std::thread& thread : late_threads ) {
+		thread.join();
+	}
+
+	mpz_class zero( 0 );
+	mpz_class &const last_val = zero;
+
+	for ( const mpz_class &const val : list ) {
+		EXPECT_GT( val, last_val );
+
+		last_val = val;
+	}
+
+	EXPECT_EQ( list.size(), (count + 1) * 9 );
+}
