ab2c440a75be089885a98e336fa41521045b8c01
BrainStone <yannick@tedworld.de>
2018-08-05 04:32:49 +0200

    Moving everything to one file

--- src/truncatablePrimes/headers/safe_queue.hpp
+++ src/truncatablePrimes/headers/safe_queue.hpp
@@ -34,4 +34,86 @@ public:
 	bool empty() const;
 };
 
-#include "safe_queue.inc.cpp"
+// Implementation
+
+template <typename T, typename container = std::deque<T>>
+safe_queue<T, container>::timeout_error::timeout_error( const std::string& what_arg )
+	: std::runtime_error( what_arg ) {};
+
+template <typename T, typename container = std::deque<T>>
+safe_queue<T, container>::timeout_error::timeout_error( const char* what_arg )
+	: std::runtime_error( what_arg ) {};
+
+template <typename T, typename container = std::deque<T>>
+void safe_queue<T, container>::push( const T & value ) {
+	{
+		std::unique_lock<std::mutex> lock( mutex );
+		queue.push_back( value );
+	}
+
+	condition.notify_one();
+}
+
+template <typename T, typename container = std::deque<T>>
+void safe_queue<T, container>::push( T && value ) {
+	{
+		std::unique_lock<std::mutex> lock( mutex );
+		queue.push_back( std::move( value ) );
+	}
+
+	condition.notify_one();
+}
+
+template <typename T, typename container = std::deque<T>>
+T safe_queue<T, container>::pop() {
+	std::unique_lock<std::mutex> lock( mutex );
+
+	condition.wait( lock, [&queue] { return !queue.empty(); } );
+
+	T out( queue.front() );
+	queue.pop_front();
+
+	return out;
+}
+
+template <typename T, typename container = std::deque<T>>
+T safe_queue<T, container>::pop( std::chrono::milliseconds timeout ) throw(timeout_error) {
+	std::unique_lock<std::mutex> lock( mutex );
+
+	if ( !condition.wait_for( lock, timeout, [&queue] { return !queue.empty(); } ) ) {
+		throw timeout_error( "No new element was added during the specified timeout of " + timeout );
+	}
+
+	T out( queue.front() );
+	queue.pop_front();
+
+	return out;
+}
+
+template <typename T, typename container = std::deque<T>>
+bool safe_queue<T, container>::tryPop( T& value, std::chrono::milliseconds timeout ) {
+	std::unique_lock<std::mutex> lock( mutex );
+
+	if ( !condition.wait_for( lock, timeout, [&queue] { return !queue.empty(); } ) ) {
+		return false;
+	}
+
+	value = queue.front();
+	queue.pop_front();
+
+	return true;
+}
+
+template <typename T, typename container = std::deque<T>>
+size_t safe_queue<T, container>::size() const {
+	std::unique_lock<std::mutex> lock( mutex );
+
+	return queue.size();
+}
+
+template<typename T, typename container>
+bool safe_queue<T, container>::empty() const {
+	std::unique_lock<std::mutex> lock( mutex );
+
+	return queue.empty();
+}
